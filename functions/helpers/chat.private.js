0861f67527bdf8e16cb29332614532779c2a8a7d
Nick Khurana <khurananick@gmail.com>
2022-06-28 10:03:47 -0400

    comments

--- functions/helpers/chat.private.js
+++ functions/helpers/chat.private.js
@@ -3,6 +3,9 @@ const helpers = require(Runtime.getFunctions()['helpers/functions'].path)();
 module.exports = function (context, event) {
   const Self = {};
 
+  /*
+   * Looks for the chat channel referenced in the inbound Chat Webhook
+   */
   Self.findChatChannel = async function(client) {
     const channel = await client.chat.v2.services(event.InstanceSid)
                 .channels(event.ChannelSid)
@@ -11,6 +14,9 @@ module.exports = function (context, event) {
     return channel;
   }
 
+  /*
+   * Fetching participants who are part of the Chat Channel
+   */
   Self.fetchChatChannelParticipants = async function(client) {
     return await client.chat.v2.services(event.InstanceSid)
                 .channels(event.ChannelSid)
@@ -18,6 +24,9 @@ module.exports = function (context, event) {
                 .list({limit: 1000})
   }
 
+  /*
+   * Updates the attributes of the Channel resource.
+   */
   Self.updateChatChannelAttributes = async function(client, params) {
     const channel = await client.chat.v2.services(event.InstanceSid)
       .channels(event.ChannelSid)
@@ -26,6 +35,9 @@ module.exports = function (context, event) {
     return channel;
   }
 
+  /*
+   * Checks if any of the participants in the Channel are set as {'member_type':'agent'}
+   */
   Self.channelHasAgent = function(participants) {
     for(let participant of participants) {
       if(participant.attributes)
@@ -34,10 +46,20 @@ module.exports = function (context, event) {
     }
   }
 
+  /*
+   * Checks if the Channel attributes include a ConversationSid attribute
+   * This would mean there is a corresponding Conversation created
+   * on the Frontline project side.
+   */
   Self.channelHasConversationMapped = function(channel) {
     return channel.attributes.ConversationSid
   }
 
+  /*
+   * By default Chat comes with a webhook that should be called on certain events.
+   * We replace the default webhook with our custom chat-to-frontline endpoint
+   * then proxy that webhook from our endpoint to continue that functionality.
+   */
   Self.replicateDefaultFlexWebhook = async function() {
     // payload to replicate Default Webhook.
     const payload = Object.assign({}, event);
--- functions/helpers/conversations.private.js
+++ functions/helpers/conversations.private.js
@@ -4,6 +4,11 @@ module.exports = function (context, event) {
   const Self = {};
   const frClient = require('twilio')(context.FRONTLINE_ACCOUNT_SID, context.FRONTLINE_AUTH_TOKEN);
 
+  /*
+   * Creates the corresponding Conversation object
+   * and sets the ChannelSid and InstanceSid of the Channel
+   * its suppose to correspond to on the Flex project
+   */
   Self.createFrontlineConversation = async function(channel, participants) {
     const convo = await frClient.conversations.conversations
         .create({friendlyName: (channel.attributes?.pre_engagement_data?.friendlyName||channel.friendlyName), attributes: JSON.stringify({
@@ -13,6 +18,10 @@ module.exports = function (context, event) {
     return convo;
   }
 
+  /*
+   * If the participant doesn't already exist in the Conversation
+   * adds the corresponding participants
+   */
   Self.addParticipantsToConversation = async function(convo, participants, channel) {
     let convoParticipants = await frClient.conversations.conversations(convo.sid)
                       .participants.list()
@@ -30,12 +39,19 @@ module.exports = function (context, event) {
     }
   }
 
+  /*
+   * Replicates the Message resource from the Channel to the Conversation
+   */
   Self.postMessageToFrontlineConversation = async function(convo, participants) {
     await frClient.conversations.conversations(convo.sid)
                       .messages
                       .create({author: event.From, body: event.Body})
   }
 
+  /*
+   * Setting the state of the Conversation as closed so it doesn't
+   * show up in the agent's frontline interface anymore.
+   */
   Self.closeFrontlineConversation = async function(convo) {
     await frClient.conversations.conversations(convo.sid)
                     .update({state: "closed"});
