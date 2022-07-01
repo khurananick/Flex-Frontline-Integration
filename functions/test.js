31bc62ecccd7fdd91d940e90510f6cb0a983133f
Nick Khurana <khurananick@gmail.com>
2022-06-30 21:30:49 -0400

    removed test file

--- functions/test.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async function (context, event, callback) {
-  const taskrouter_helpers = require(Runtime.getFunctions()['helpers/taskrouter'].path)(context, event);
-  const client = context.getTwilioClient();
-  const workers = await taskrouter_helpers.getActivityByName(client, context.WORKSPACE_SID, 'Unavailable');
-  callback(null, workers);
-}
